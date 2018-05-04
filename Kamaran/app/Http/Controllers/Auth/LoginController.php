27ff803acd653f60cc34d5d44682fe0889ab9160
Hisham Ali <ahishamali10@gmail.com>
2018-05-04 23:58:07 +0300

    [login]  altering the login options, validation, view, and route

--- Kamaran/app/Http/Controllers/Auth/LoginController.php
+++ Kamaran/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -36,4 +37,16 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    protected function credentials(Request $request)
+    {
+        return array_merge($request->only($this->username(), 'password'), ['status' => 1]);
+    }
+
+    public function username()
+    {
+        return 'username';
+    }
+
+
 }
