890048fb7ffae3ea435d158ad2fc10bd5002230c
Yousef <yhadhr@gmail.com>
2018-07-17 23:35:15 +0300

    [view] added manage items

---
+++ Kamaran/resources/views/manage_items.blade.php
@@ -0,0 +1,84 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Manage Items</h1>
+
+@stop
+
+@section('content')
+
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Items</h3>
+
+                    <div class="box-tools">
+                        <div class="input-group input-group-sm" style="width: 150px;">
+                            <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+                            <div class="input-group-btn">
+                                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body table-responsive no-padding">
+                    <table class="table table-hover">
+                        <tbody><tr>
+                            <th>Item ID</th>
+                            <th>Item Name</th>
+                            <th>Description</th>
+                            <th>Specification</th>
+                            <th>Unit</th>
+                            <th>Danger Level</th>
+                            <th>Type</th>
+                        </tr>
+                        <tr>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td><span class="label label-success">data</span></td>
+                            <td>data</td>
+                        </tr>
+                        <tr>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td><span class="label label-success">data</span></td>
+                            <td>data</td>
+                        </tr>
+                        <tr>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td><span class="label label-success">data</span></td>
+                            <td>data</td>
+                        </tr>
+                        <tr>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td>data</td>
+                            <td><span class="label label-success">data</span></td>
+                            <td>data</td>   
+                        </tr>
+                        </tbody></table>
+                </div>
+                <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+        </div>
+    </div>
+
+@stop
