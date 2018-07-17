181e9a558d99c480bcf5799bce9a7abbad18dfaa
Yousef <yhadhr@gmail.com>
2018-07-17 23:05:15 +0300

    [view] added fill shipment

--- Kamaran/resources/views/fill_shipment.blade.php
+++ Kamaran/resources/views/fill_shipment.blade.php
@@ -1,7 +1,68 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Yousef
- * Date: 7/17/2018
- * Time: 9:35 PM
- */
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Make a Shippment</h1>
+
+@stop
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-primary">
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Shipment details:</h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <!-- form start -->
+                        <form role="form" _lpchecked="1">
+                            <div class="box-body">
+                                <div class="form-group">
+                                    <label for="">Arrival Date:</label>
+                                    <div class="input-group date">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-calendar"></i>
+                                        </div>
+                                        <input type="text" class="form-control pull-right" id="datepicker">
+                                    </div>
+                                </div>
+                                <div class="input-group input-group-sm">
+                                    <label for="">Order:</label>
+                                    <input type="text" disabled="" class="form-control">
+                                    <span class="input-group-btn">
+                                    <button type="button" class="btn btn-info btn-flat">Browse</button>
+                                </span>
+                                </div>
+                                <div>
+                                    Quantity
+                                </div>
+
+                                <div>
+                                    Item Name:
+                                </div>
+
+                                <div>
+                                    Danger Level:
+                                </div>
+
+                            <!-- /.box-body -->
+
+                            <div class="box-footer">
+                                <button type="clear" class="btn btn-default ">Cancel</button>
+                                <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-2"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@stop
