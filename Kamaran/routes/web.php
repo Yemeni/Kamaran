d721f6b6f58ea6a9eb922ac2e6a0741d7876ef01
Yousef <yhadhr@gmail.com>
2018-07-17 23:04:38 +0300

    [route] added routes

--- Kamaran/routes/web.php
+++ Kamaran/routes/web.php
@@ -50,12 +50,71 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+//Route::get('/', 'HomeController@index')->name('home');
+
+
+// Temporally views for debugging
+// Dashboard
+Route::get('/overview', function (){
+    return view('overview');
+});
+
+Route::get('/performance', function (){
+    return view('performance');
+});
+
+Route::get('/inventory', function (){
+    return view('inventory');
+});
+
+Route::get('/orders', function (){
+    return view('orders');
+});
+
+Route::get('/shipments', function (){
+    return view('shipments');
+});
+
+// Apps
+
+Route::get('/review_orders', function (){
+    return view('review_orders');
+});
+
+Route::get('/fill_order', function (){
+    return view('fill_order');
+});
+
+Route::get('/track_shipments', function (){
+    return view('track_shipments');
+});
+
+Route::get('/fill_shipment', function (){
+    return view('fill_shipment');
+});
+
+Route::get('/manage_items', function (){
+    return view('manage_items');
+});
+
+Route::get('/manage_suppliers', function (){
+    return view('manage_suppliers');
+});
+
+Route::get('/print_reports', function (){
+    return view('print_reports');
+});
+
+// Staff Management
 
 Route::get('/profile', function (){
     return view('profile');
 });
 
-Route::get('/fill_order', function (){
-    return view('fill_order');
+Route::get('/employees', function (){
+    return view('employees');
+});
+
+Route::get('/my_manager', function (){
+    return view('my_manager');
 });
