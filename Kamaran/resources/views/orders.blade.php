f28f33194be1a0f3233ccc65c8ea0ab5fdeda162
Yousef <yhadhr@gmail.com>
2018-07-27 20:51:16 +0300

    [view] minor fixes for orders

--- Kamaran/resources/views/orders.blade.php
+++ Kamaran/resources/views/orders.blade.php
@@ -4,6 +4,9 @@
 
 @section('content_header')
     <h1>Orders</h1>
+@stop
+
+@section('content')
     <div class="row">
         <!-- ./col -->
         <div class="col-lg-4 col-xs-6">
@@ -114,144 +117,140 @@
         </div>
         <!-- ./col -->
 
-            <div class="col-lg-2 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-aqua">
-                    <div class="inner">
-                        <h3>80</h3>
+        <div class="col-lg-2 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>80</h3>
 
-                        <p>Pending Orders</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    <p>Pending Orders</p>
                 </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <!-- ./col -->
-            <div class="col-lg-2 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-green">
-                    <div class="inner">
-                        <h3>53</h3>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-2 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>53</h3>
 
-                        <p>Approved Orders</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    <p>Approved Orders</p>
                 </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <!-- ./col -->
-            <div class="col-lg-2 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-purple">
-                    <div class="inner">
-                        <h3>8</h3>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-2 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-purple">
+                <div class="inner">
+                    <h3>8</h3>
 
-                        <p>Cancelled Orders</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    <p>Cancelled Orders</p>
                 </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <!-- .col -->
-            <div class="col-lg-2 col-xs-6">
-                <!-- small box -->
-                <div class="box">
-                    <div class="inner">
-                        <p>Today is Tuesday,</p>
-                        <p>22 March 2019</p>
-                        <p>8:31 AM</p>
-                    </div>
+        </div>
+        <!-- .col -->
+        <div class="col-lg-2 col-xs-6">
+            <!-- small box -->
+            <div class="box">
+                <div class="inner">
+                    <p>Today is Tuesday,</p>
+                    <p>22 March 2019</p>
+                    <p>8:31 AM</p>
+                </div>
 
-                </div>
             </div>
-            <!-- ./col -->
+        </div>
+        <!-- ./col -->
 
 
     </div>
 
     <div class="row">
         <div class="col-xs-12">
-                <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">Orders Table</h3>
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Orders Table</h3>
 
-                        <div class="box-tools">
-                            <div class="input-group input-group-sm" style="width: 150px;">
-                                <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+                    <div class="box-tools">
+                        <div class="input-group input-group-sm" style="width: 150px;">
+                            <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
 
-                                <div class="input-group-btn">
-                                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                                </div>
+                            <div class="input-group-btn">
+                                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                             </div>
                         </div>
                     </div>
-                    <!-- /.box-header -->
-                    <div class="box-body table-responsive no-padding">
-                        <table class="table table-hover">
-                            <tbody><tr>
-                                <th>Order ID</th>
-                                <th>Status</th>
-                                <th>Date</th>
-                                <th>Letter of Credit</th>
-                                <th>Approval date</th>
-                                <th>Usesr</th>
-                                <th>Suppliers</th>
-                                <th>Category</th>
-                                <th>Comment</th>
-                            </tr>
-                            <tr>
-                                <td>183</td>
-                                <td>Ahmed Ali</td>
-                                <td>11-7-2018</td>
-                                <td>11-7-2018</td>
-                                <td>CIF</td>
-                                <td><span class="label label-success">Approved</span></td>
-                                <td>They will give 5% discount the next time we order</td>
-                                <td>hi</td>
-                                <td>They will give 5% discount the next time we order</td>
-                            </tr>
-                            <tr>
-                                <td>219</td>
-                                <td>Alexander Mohammed</td>
-                                <td>11-7-2018</td>
-                                <td>11-7-2018</td>
-                                <td>CIF</td>
-                                <td><span class="label label-warning">Pending</span></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>657</td>
-                                <td>Ibtisam Doe</td>
-                                <td>11-7-2018</td>
-                                <td>11-7-2018</td>
-                                <td>FOB</td>
-                                <td><span class="label label-success">Approved</span></td>
-                                <td>This is very necessary for the XVR machine</td>
-                            </tr>
-                            <tr>
-                                <td>175</td>
-                                <td>Fatimah Salah</td>
-                                <td>11-7-2018</td>
-                                <td>11-7-2018</td>
-                                <td>CIF</td>
-                                <td><span class="label label-danger">Cancelled</span></td>
-                                <td></td>
-                            </tr>
-                            </tbody></table>
-                    </div>
-                    <!-- /.box-body -->
                 </div>
-                <!-- /.box -->
+                <!-- /.box-header -->
+                <div class="box-body table-responsive no-padding">
+                    <table class="table table-hover">
+                        <tbody><tr>
+                            <th>Order ID</th>
+                            <th>Status</th>
+                            <th>Date</th>
+                            <th>Letter of Credit</th>
+                            <th>Approval date</th>
+                            <th>Usesr</th>
+                            <th>Suppliers</th>
+                            <th>Category</th>
+                            <th>Comment</th>
+                        </tr>
+                        <tr>
+                            <td>183</td>
+                            <td>Ahmed Ali</td>
+                            <td>11-7-2018</td>
+                            <td>11-7-2018</td>
+                            <td>CIF</td>
+                            <td><span class="label label-success">Approved</span></td>
+                            <td>They will give 5% discount the next time we order</td>
+                            <td>hi</td>
+                            <td>They will give 5% discount the next time we order</td>
+                        </tr>
+                        <tr>
+                            <td>219</td>
+                            <td>Alexander Mohammed</td>
+                            <td>11-7-2018</td>
+                            <td>11-7-2018</td>
+                            <td>CIF</td>
+                            <td><span class="label label-warning">Pending</span></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>657</td>
+                            <td>Ibtisam Doe</td>
+                            <td>11-7-2018</td>
+                            <td>11-7-2018</td>
+                            <td>FOB</td>
+                            <td><span class="label label-success">Approved</span></td>
+                            <td>This is very necessary for the XVR machine</td>
+                        </tr>
+                        <tr>
+                            <td>175</td>
+                            <td>Fatimah Salah</td>
+                            <td>11-7-2018</td>
+                            <td>11-7-2018</td>
+                            <td>CIF</td>
+                            <td><span class="label label-danger">Cancelled</span></td>
+                            <td></td>
+                        </tr>
+                        </tbody></table>
+                </div>
+                <!-- /.box-body -->
             </div>
+            <!-- /.box -->
+        </div>
     </div>
-@stop
-
-@section('content')
-
 @stop
