27df15ecda641a9be8873e400cd8afc134d5c809
Yousef <yhadhr@gmail.com>
2018-07-28 22:08:42 +0300

    [view] employee finalizing

--- Kamaran/resources/views/employee.blade.php
+++ Kamaran/resources/views/employee.blade.php
@@ -45,65 +45,38 @@
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label>Description</label>
-                                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label>Specification</label>
-                                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Unit:</label>
-                                    <select class="form-control">
-                                        <option>option 1</option>
-                                        <option>option 2</option>
-                                        <option>option 3</option>
-                                        <option>option 4</option>
-                                        <option>option 5</option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Danger Level:</label>
-                                    <select class="form-control">
-                                        <option>option 1</option>
-                                        <option>option 2</option>
-                                        <option>option 3</option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">Type:</label>
+                                    <label for="">Phone:</label>
                                     <input type="text" class="form-control" id="">
                                 </div>
-                                <div></div>
-
-                                <!-- Custom Tabs -->
-                                <div class="nav-tabs-custom">
-                                    <ul class="nav nav-tabs">
-                                        <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true">Supplier 1</a></li>
-                                        <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">Supplier 2</a></li>
-                                        <li class=""><a href="#tab_3" data-toggle="tab" aria-expanded="false">Supplier 3</a></li>
-                                        <li class="pull-right"><a href="#" class="text-muted"><i class="fa fa-plus"></i> Add Supplier</a></li>
-                                    </ul>
-                                    <div class="tab-content">
-                                        <div class="tab-pane active" id="tab_1">
-                                            <div class="form-group">
-                                                <label for="">Supplier Name:</label>
-                                                <input type="text" disabled="" class="form-control" id="">
-                                            </div>
-                                        </div>
-                                        <!-- /.tab-pane -->
-                                        <div class="tab-pane" id="tab_2">
-
-                                        </div>
-                                        <!-- /.tab-pane -->
-                                        <div class="tab-pane" id="tab_3">
-
-                                        </div>
-                                        <!-- /.tab-pane -->
-                                    </div>
-                                    <!-- /.tab-content -->
+                                <div class="form-group">
+                                    <label for="">Email:</label>
+                                    <input type="text" class="form-control" id="">
                                 </div>
-                                <!-- nav-tabs-custom -->
+                                <div class="form-group">
+                                    <label for="">Level:</label>
+                                    <select class="form-control">
+                                        <option>Admin</option>
+                                        <option>Manager</option>
+                                        <option>Employee</option>
+                                        <option>Inventory Employee</option>
+                                        <option>Head of Suppliers</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Address</label>
+                                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" checked>
+                                            Active
+                                        </label>
+                                    </div>
+
+
+
+                                </div>
 
                             </div>
                             <!-- /.box-body -->
