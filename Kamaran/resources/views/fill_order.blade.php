f805328d0523db0f1033fa1d5fea924787dc0e96
Yousef <yhadhr@gmail.com>
2018-07-31 21:28:02 +0300

    [view+js] added select.js to items and auto generate tabs and content when clicking add new item

--- Kamaran/resources/views/fill_order.blade.php
+++ Kamaran/resources/views/fill_order.blade.php
@@ -66,16 +66,89 @@
                             <div class="nav-tabs-custom">
                                 <ul class="nav nav-tabs">
                                     <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true">Item 1</a></li>
-                                    <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">Item 2</a></li>
-                                    <li class=""><a href="#tab_3" data-toggle="tab" aria-expanded="false">Item 3</a></li>
-                                    <li class="pull-right"><a href="#" class="text-muted"><i class="fa fa-plus"></i> Add Item</a></li>
+                                    <li class="pull-right add-tab"><a href="#" class="text-muted"><i class="fa fa-plus"></i> Add Item</a></li>
                                 </ul>
                                 <div class="tab-content">
                                     <div class="tab-pane active" id="tab_1">
                                         <div class="form-group">
                                             <label for="">Item:</label>
-                                                <input type="text" disabled="" class="form-control" id="">
+                                            <select class="js-example-basic-single-item form-control">
+                                                <option value="">Tobacco xyz</option>
+                                                <option value="">Something xyz</option>
+                                            </select>
                                         </div>
+                                        @section('js')
+                                            <script>
+                                                // select2 auto complete
+                                                $(document).ready(function() {
+                                                    $('.js-example-basic-single-item').select2();
+                                                });
+
+                                                // add new tab
+                                                $(".nav-tabs").on("click", "a", function (e) {
+                                                    e.preventDefault();
+                                                    if (!$(this).hasClass('add-tab')) {
+                                                        $(this).tab('show');
+                                                    }
+                                                })
+                                                    .on("click", "span", function () {
+                                                        var anchor = $(this).siblings('a');
+                                                        $(anchor.attr('href')).remove();
+                                                        $(this).parent().remove();
+                                                        $(".nav-tabs li").children('a').first().click();
+                                                    });
+
+                                                $('.add-tab').click(function (e) {
+                                                    e.preventDefault();
+                                                    var id = $(".nav-tabs").children().length; //think about it ;)
+                                                    var tabId = 'tab_' + id;
+                                                    $(this).closest('li').before('<li><a href="#tab_' + id + '">Supplier ' + id +'</a> <span> x </span></li>');
+                                                    $('.tab-content').append('<div class="tab-pane" id="' + tabId + '">' +
+                                                        '<div class="form-group">' +
+                                                            '<label for="">Supplier Name:</label>' +
+                                                            '<select class="js-example-basic-single-item'+id +' form-control">' +
+                                                                '<option value="">Tobbaco XYZ</option>' + // repopulate from database
+                                                                '<option value="">Fire ABC</option>' +
+                                                            '</select>' +
+                                                        '</div>' +
+                                                        '<div class="form-group">' +
+                                                            '<label for="">Quantity:</label>' +
+                                                            '<input type="text" class="form-control" id="">' +
+                                                        '</div>' +
+                                                        '<div class="form-group">' +
+                                                        '<label for="">Cost:</label>' +
+                                                        '<input type="text" class="form-control" id="">' +
+                                                        '</div>' +
+                                                        '</div>');
+                                                    $('.nav-tabs li:nth-child(' + id + ') a').click();
+
+                                                    $(document).ready(function() {
+                                                        $('.js-example-basic-single-item'+id).select2();
+                                                    });
+                                                });
+                                            </script>
+                                            @append
+                                        @section('css')
+                                            <style>
+                                                .nav-tabs > li {
+                                                    position:relative;
+                                                }
+                                                .nav-tabs > li > a {
+                                                    display:inline-block;
+                                                }
+                                                .nav-tabs > li > span {
+                                                    display:none;
+                                                    cursor:pointer;
+                                                    position:absolute;
+                                                    right: 6px;
+                                                    top: 8px;
+                                                    color: red;
+                                                }
+                                                .nav-tabs > li:hover > span {
+                                                    display: inline-block;
+                                                }
+                                            </style>
+                                        @append
                                         <div class="form-group">
                                             <label for="">Quantity:</label>
                                             <input type="text" class="form-control" id="">
@@ -84,15 +157,6 @@
                                             <label for="">Cost:</label>
                                             <input type="text" class="form-control" id="">
                                         </div>
-                                    </div>
-                                    <!-- /.tab-pane -->
-                                    <div class="tab-pane" id="tab_2">
-
-                                    </div>
-                                    <!-- /.tab-pane -->
-                                    <div class="tab-pane" id="tab_3">
-
-                                    </div>
                                     <!-- /.tab-pane -->
                                 </div>
                                 <!-- /.tab-content -->
