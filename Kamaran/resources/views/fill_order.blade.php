fe2fc92598bcd7fba13cdc34cbcf62987b7a8798
Hisham <ahishamali10@gmail.com>
2018-08-15 00:27:04 +0300

    finishing fill order view

--- Kamaran/resources/views/fill_order.blade.php
+++ Kamaran/resources/views/fill_order.blade.php
@@ -9,125 +9,124 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="box box-primary">
-            <div class="row">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Order details:</h3>
-                    </div>
-                    <!-- /.box-header -->
-                    <!-- form start -->
-                    <form role="form" _lpchecked="1">
-                        <div class="box-body">
-                            <div class="form-group">
-                                <label for="">Date:</label>
-                                <div class="input-group date">
-                                    <div class="input-group-addon">
-                                        <i class="fa fa-calendar"></i>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-primary">
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-8">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Order details:</h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <!-- form start -->
+                        <form role="form" action="{{ url('/order') }}" method="post">
+                            @csrf
+
+                            <div class="box-body">
+                                <div class="form-group">
+                                    <label for="">Date:</label>
+                                    <div class="input-group date">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-calendar"></i>
+                                        </div>
+                                        <input type="text"
+                                               name="date"
+                                               class="form-control pull-right input-append date form_datetime"
+                                               id="">
                                     </div>
-                                    <input type="text" class="form-control pull-right input-append date form_datetime" id="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="">Letter:</label>
+                                    <select name="letter_of_credit" class="form-control">
+                                        <option>cif</option>
+                                        <option>cf</option>
+                                        <option>fob</option>
+                                        <option>cfr</option>
+                                        <option>other</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Supplier Name:</label>
+                                    <select name="supplier_id" class="js-example-basic-single form-control">
+                                        <option selected disabled> Select a Supplier</option>
+                                        @foreach($suppliers as $supplier)
+                                            <option value="{{ $supplier->id }}">{{ $supplier->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Comment</label>
+                                    <textarea name="comment" class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                                </div>
+                                <div></div>
+
+                                <div class="form-group">
+                                    <label for="">Item:</label>
+                                    <select name="item_id" class="js-example-basic-single-item form-control">
+                                        <option selected disabled>Choose the Supplier first</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="">Quantity:</label>
+                                    <input name="quantity" type="number" class="form-control calc" id="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Cost:</label>
+                                    <input type="number" name="cost" class="form-control calc" id="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Total Cost:</label>&nbsp;
+                                    <span id="total">0</span> $
+                                </div>
+
+                            <!-- /.box-body -->
+
+                                <div class="box-footer">
+                                    <a href="{{ url('/review_orders') }}" class="btn btn-default">Cancel</a>
+                                    <button type="submit" class="btn btn-primary pull-right">Submit</button>
                                 </div>
                             </div>
-                            @section('js')
-                                <script type="text/javascript">
-                                    $(".form_datetime").datetimepicker({
-                                        format: "dd MM yyyy - hh:ii"
-                                    });
-                                </script>
-                            @append
-
-                            <div class="form-group">
-                                <label for="">Letter:</label>
-                                <select class="form-control">
-                                    <option>cif</option>
-                                    <option>cf</option>
-                                    <option>fob</option>
-                                    <option>cfr</option>
-                                    <option>other</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="">Supplier Name:</label>
-                                <select class="js-example-basic-single form-control">
-                                    <option value="">Something .co</option>
-                                    <option value="">Company XYZ .limted</option>
-                                </select>
-                            </div>
-                            @section('js')
-                                <script>
-                                    // select2 auto complete
-                                    $(document).ready(function() {
-                                        $('.js-example-basic-single').select2();
-                                    });
-                                </script>
-                            @append
-                            <div class="form-group">
-                                <label>Comment</label>
-                                <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                            </div>
-                            <div></div>
-
-                            <div class="form-group">
-                                <label for="">Item:</label>
-                                <select class="js-example-basic-single-item form-control">
-                                    <option value="">Tobacco xyz</option>
-                                    <option value="">Something xyz</option>
-                                </select>
-                            </div>
-                            @section('js')
-                                <script>
-                                    // select2 auto complete
-                                    $(document).ready(function() {
-                                        $('.js-example-basic-single-item').select2();
-                                    });
-                                </script>
-                                @append
-
-                            <div class="form-group">
-                                <label for="">Quantity:</label>
-                                <input type="text" class="form-control calc" id="">
-                            </div>
-                            <div class="form-group">
-                                <label for="">Cost:</label>
-                                <input type="text" class="form-control calc" id="">
-                            </div>
-                            <div class="form-group">
-                                <label for="">Total Cost:</label>
-                                <span id="total"></span>
-                            </div>
-                            @section('js')
-                                <script>
-                                    $(document).ready(function(){
-                                        $('.calc').change(function(){
-                                            var total = 1;
-                                            $('.calc').each(function(){
-                                                if($(this).val() != '')
-                                                {
-                                                    total *= parseInt($(this).val());
-                                                }
-                                            });
-                                            $('#total').html(total);
-                                        });
-                                    });
-                                </script>
-                                @append
-
-                        <!-- /.box-body -->
-
-                        <div class="box-footer">
-                            <button type="clear" class="btn btn-default ">Cancel</button>
-                            <button type="submit" class="btn btn-primary pull-right">Submit</button>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
+                    <div class="col-md-2"></div>
                 </div>
-                <div class="col-md-2"></div>
             </div>
         </div>
     </div>
-</div>
 
 
 @stop
+
+@section('adminlte_js')
+
+    <script>
+        $(document).ready(function () {
+
+            $('.calc').change(function () {
+                var total = 1;
+                $('.calc').each(function () {
+                    if ($(this).val() != '') {
+                        total *= parseInt($(this).val());
+                    }
+                });
+                $('#total').html(total);
+            });
+
+            $(".form_datetime").datetimepicker();
+
+                $('.js-example-basic-single').select2();
+
+            $('select[name="supplier_id"]').on('change.select2', function (eve) {
+                $.get("/supplier/"+$(this).val()+"/items", function(data, status){
+                    $('.js-example-basic-single-item').select2({
+                        data: JSON.parse(data)
+                    });
+                });
+            })
+        })
+    </script>
+
+@endsection
