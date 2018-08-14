d9616111077363a77806abd1e3f654e35cb240e8
Yousef <yhadhr@gmail.com>
2018-08-14 19:52:05 +0300

    Revert "[migration] bug, removed extra spaces"
    This reverts commit 6b5c7e339efb3218fd15d46010be5717c5cab355.

--- Kamaran/database/migrations/2018_05_04_184051_create_categories_table.php
+++ Kamaran/database/migrations/2018_05_04_184051_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
-            $table->text('comment')->nullable();
+            $table->string('name')->unique()	;
+            $table->text('comment')->nullabl	e();
             $table->timestamps();
         });
     }
