eff5c77b0692e2fdeb984a141569848f8d8ba361
Yousef <yhadhr@gmail.com>
2018-08-15 02:11:43 +0300

    [view] added error page

---
+++ Kamaran/resources/views/error.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Error</h1>
+
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <h2>Unauthorized access</h2>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+
+@stop
