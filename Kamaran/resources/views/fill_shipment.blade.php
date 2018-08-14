eb71759325cb9d176fff980b25aead91e567211e
Yousef <yhadhr@gmail.com>
2018-08-14 23:20:43 +0300

    [view] shipment manual data view

--- Kamaran/resources/views/fill_shipment.blade.php
+++ Kamaran/resources/views/fill_shipment.blade.php
@@ -40,43 +40,12 @@
                                         <input type="text" class="form-control pull-right" id="datepicker">
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="">Order:</label>
-                                    <select class="js-example-basic-single-order form-control">
-                                        <option value="">114</option>
-                                        <option value="">116</option>
-                                    </select>
-                                </span>
-                                </div>
-                                @section('js')
-                                    <script>
-                                        // select2 auto complete
-                                        $(document).ready(function() {
-                                            $('.js-example-basic-single-order').select2();
-                                        });
-                                    </script>
-                                @append
+
                                 <div class="form-group">
                                     <label for="">Quantity:</label>
                                     <input type="text" class="form-control" id="">
                                 </div>
 
-                                <div class="form-group">
-                                    <label for="">Item Name:</label>
-                                    <select class="js-example-basic-single-item form-control">
-                                        <option value="">Tobacco xyz</option>
-                                        <option value="">Something xyz</option>
-                                    </select>
-                                </span>
-                                </div>
-                                @section('js')
-                                    <script>
-                                        // select2 auto complete
-                                        $(document).ready(function() {
-                                            $('.js-example-basic-single-item').select2();
-                                        });
-                                    </script>
-                                @append
                                 <div class="form-group">
                                     <label for="">Danger Level:</label>
                                     <input type="text" disabled="" value="Flammable" class="form-control" id="">
