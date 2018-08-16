cd935b3b94007f7858305e33768ee2d79132d453
Yousef <yhadhr@gmail.com>
2018-08-16 04:10:03 +0300

    [controler] added auth to these controllers

--- Kamaran/app/Http/Controllers/ItemController.php
+++ Kamaran/app/Http/Controllers/ItemController.php
@@ -12,6 +12,11 @@ use Illuminate\Validation\Rule;
 
 class ItemController extends Controller {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
 	/**
 	 * Display a listing of the resource.
 	 *
--- Kamaran/app/Http/Controllers/SupplierController.php
+++ Kamaran/app/Http/Controllers/SupplierController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Gate;
 
 class SupplierController extends Controller {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
 	/**
 	 * Display a listing of the resource.
 	 *
