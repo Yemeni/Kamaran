e638f9e773d0970bfe38906ac3f90f8b55abc485
Yousef <yhadhr@gmail.com>
2018-08-18 02:45:14 +0300

    [task] Issue #39

--- Kamaran/resources/views/about.blade.php
+++ Kamaran/resources/views/about.blade.php
@@ -11,40 +11,52 @@
     <div class="row">
         <div class="col-md-2"></div>
         <div class="col-md-8">
-            <h2>Supply Inventory Management System
-                for
-                Kamaran Industry & Investment Co.
-            </h2>
-            <p>
-                Developed by:
+            <div align="center">
+                <h2>Supply Inventory Management System <small>Version 0.9.0</small><br/>
+                    for<br/>
+                    Kamaran Industry & Investment Co.
+                </h2>
+            </div>
+            <br/><br/>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>
+                        Developed by:
 
 
-            </p>
-            <ul>
-                <li>Yousef Al-Hadhrami</li>
-                <li>Rashad H. Al-Khmisy</li>
-                <li>Anwar H.Sharhan</li>
-            </ul>
+                    </p>
+                    <ul>
+                        <li>Yousef Al-Hadhrami</li>
+                        <li>Rashad H. Al-Khmisy</li>
+                        <li>Anwar H.Sharhan</li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <p>To get help, you can do the following:</p>
 
-            <p>To get help, you can do the following:</p>
+                    <strong><i class="fa fa-phone margin-r-5"></i> Call us on:</strong>
 
-            <strong><i class="fa fa-phone margin-r-5"></i> Call us on:</strong>
+                    <p class="text-muted">
+                        01-440030/23
+                    </p>
 
-            <p class="text-muted">
-                01-440030/23
-            </p>
+                    <strong><i class="fa fa-phone-square margin-r-5"></i> Ext:</strong>
 
-            <strong><i class="fa fa-phone-square margin-r-5"></i> Ext:</strong>
+                    <p class="text-muted">
+                        143-144
+                    </p>
 
-            <p class="text-muted">
-                143-144
-            </p>
+                    <strong><i class="fa fa-envelope margin-r-5"></i> Or please connect us on::</strong>
 
-            <strong><i class="fa fa-envelope margin-r-5"></i> Or please connect us on::</strong>
+                    <p class="text-muted">
+                        support@kamaran.com
+                    </p>
 
-            <p class="text-muted">
-                support@kamaran.com
-            </p>
+                </div>
+            </div>
+
+
+
         </div>
         <div class="col-md-2"></div>
     </div>
