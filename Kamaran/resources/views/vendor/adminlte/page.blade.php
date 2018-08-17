f037510a73ee60fc050157638d3a10976ab4d5a4
Yousef <yhadhr@gmail.com>
2018-08-17 20:26:19 +0300

    show logged in user name on the navbar

--- Kamaran/resources/views/vendor/adminlte/page.blade.php
+++ Kamaran/resources/views/vendor/adminlte/page.blade.php
@@ -57,6 +57,7 @@
                 <div class="navbar-custom-menu">
 
                     <ul class="nav navbar-nav">
+                        <li><a>Hello {{ $user->name }}</a></li>
                         <li>
                             @if(config('adminlte.logout_method') == 'GET' || !config('adminlte.logout_method') && version_compare(\Illuminate\Foundation\Application::VERSION, '5.3.0', '<'))
                                 <a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}">
