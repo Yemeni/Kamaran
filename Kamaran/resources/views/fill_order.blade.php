81e9127b14a2398769e65b1d006d039c34e1e2b8
Yousef <yhadhr@gmail.com>
2018-08-17 21:02:48 +0300

    return float instead of int when filling order

--- Kamaran/resources/views/fill_order.blade.php
+++ Kamaran/resources/views/fill_order.blade.php
@@ -134,7 +134,7 @@
                 var total = 1;
                 $('.calc').each(function () {
                     if ($(this).val() != '') {
-                        total *= parseInt($(this).val());
+                        total *= parseFloat($(this).val());
                     }
                 });
                 $('#total').html(total);
