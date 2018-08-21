743e982f8265926503a2bfb156e016f3d535f9f7
Yousef <yhadhr@gmail.com>
2018-08-21 22:30:26 +0300

    [code] Issue #80 mixing the dependencies js, css, sass & less to one js and css

--- Kamaran/resources/assets/js/app.js
+++ Kamaran/resources/assets/js/app.js
@@ -6,8 +6,11 @@
  */
 
 require('./bootstrap');
-
-window.Vue = require('vue');
+require('select2');
+require('datetimepicker');
+require('datatables');
+require('chart.js');
+// window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +18,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//
+// const app = new Vue({
+//     el: '#app'
+// });
--- Kamaran/webpack.mix.js
+++ Kamaran/webpack.mix.js
@@ -11,5 +11,18 @@ let mix = require('laravel-mix');
  |
  */
 
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .less('resources/assets/less/app.less', 'public/css');
+
+mix.copy('node_modules/datatables/media/css/jquery.dataTables.min.css', 'public/css/datatables.css');
+mix.copy('node_modules/datatables/media/images', 'public/images', false );
+
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css/app-scss.css')
+    .less('resources/assets/less/app.less', 'public/css/app-less.css')
+    .styles([
+        'public/css/app-scss.css',
+        'public/css/app-less.css',
+        'public/css/datatables.css',
+    ], 'public/css/app.css');
