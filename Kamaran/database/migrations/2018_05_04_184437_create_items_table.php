46c0f29c6b79aa401c3a10d85a4db1dece0b2573
Yousef <yhadhr@gmail.com>
2018-08-23 23:47:32 +0300

    Updated migration for items

--- Kamaran/database/migrations/2018_05_04_184437_create_items_table.php
+++ Kamaran/database/migrations/2018_05_04_184437_create_items_table.php
@@ -21,6 +21,7 @@ class CreateItemsTable extends Migration
             $table->text('specification')->nullable();
             $table->enum('unit', ['KG','Gram','Tonne','Liter','Milliliter','Barrel','Gallon','Bottle','Meter','Centimeter','Kilometer','Cartons','Pack','Packet','Box']);
             $table->enum('danger_level', ['low','flammable','toxic']);
+            $table->boolean('important')->default(false);
             $table->timestamps();
         });
     }
